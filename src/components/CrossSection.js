1ec740fbeabbba344bc7d22c251839b24008368a
Chris <chrispdunne@gmail.com>
2019-09-29 19:38:41 +0100

    sweet cutaway

--- src/components/CrossSection.js
+++ src/components/CrossSection.js
@@ -2,9 +2,10 @@ import React from "react"
 import styled from "styled-components"
 import { Controller, Scene } from "react-scrollmagic"
 import { Tween, Timeline } from "react-gsap"
+import Particles from "react-particles-js"
 
-import bgImg from "../images/isometic-underlay.jpg"
-import fgImg from "../images/isometic-overlay.png"
+import bgImg from "../images/isometic-underlay2.png"
+import fgImg from "../images/isometic-overlay2.png"
 
 const CrossSectionStyled = styled.div`
     overflow: hidden;
@@ -13,16 +14,26 @@ const CrossSectionStyled = styled.div`
         width: 100vw;
         overflow: hidden;
         .panel {
-            height: 100vh;
-            width: 100vw;
-            position: absolute;
-            text-align: center;
+            position: fixed;
+            top: 50%;
+            transform: translateY(-50%);
+            right: 0rem;
+            text-align: right;
+            background-color: rgba(0, 0, 0, 0.7);
+            padding: 1rem 1rem 1rem;
+            margin-right: 1rem;
+            span {
+                color: #fff;
+                display: block;
+                max-width: 12rem;
+            }
         }
     }
     .bg {
         background-image: url(${bgImg});
         background-size: contain;
         background-repeat: no-repeat;
+        /* background-position: center; */
         width: 100%;
         height: 100%;
     }
@@ -32,14 +43,24 @@ const CrossSectionStyled = styled.div`
         background-image: url(${fgImg});
         background-size: contain;
         background-repeat: no-repeat;
+        /* background-position: center; */
         width: 100%;
         height: 100%;
     }
-    .panel span {
-        position: relative;
-        display: block;
-        top: 50%;
-        font-size: 80px;
+    .vignette {
+        background: radial-gradient(rgba(255, 255, 255, 0), rgba(0, 0, 0, 0.6));
+        position: fixed;
+        height: 100vh;
+        width: 100vw;
+        z-index: 2;
+        top: 0;
+    }
+    .particles {
+        position: fixed;
+        z-index: 3;
+        top: 0;
+        height: 100vh;
+        width: 100vw;
     }
 `
 
@@ -50,18 +71,59 @@ const CrossSection = () => (
                 <Timeline wrapper={<div id="pinContainer" />}>
                     <div className=" bg" />
 
-                    <Tween from={{ x: "0%" }} to={{ x: "30%" }}>
+                    <Tween from={{ y: "0%" }} to={{ y: "-50%" }}>
                         <div className=" fg" />
                     </Tween>
 
-                    <Tween from={{ x: "100%" }} to={{ x: "0%" }}>
-                        <section className="panel green">
-                            <span>This</span>
+                    <Tween from={{ opacity: "0" }} to={{ opacity: "1" }}>
+                        <section className="panel intro">
+                            <span>
+                                This cut away look at a building rocks your
+                                socks so it does hear ye hear ye.
+                            </span>
                         </section>
                     </Tween>
                 </Timeline>
             </Scene>
         </Controller>
+        {/* <div className="vignette" /> */}
+        {/* <Particles
+            className="particles"
+            params={{
+                particles: {
+                    number: {
+                        value: 160,
+                        density: {
+                            enable: false,
+                        },
+                    },
+                    size: {
+                        value: 2,
+                        random: true,
+                    },
+                    move: {
+                        direction: "bottom",
+                        out_mode: "out",
+                    },
+                    line_linked: {
+                        enable: false,
+                    },
+                },
+                interactivity: {
+                    events: {
+                        onclick: {
+                            enable: true,
+                            mode: "remove",
+                        },
+                    },
+                    modes: {
+                        remove: {
+                            particles_nb: 10,
+                        },
+                    },
+                },
+            }}
+        /> */}
     </CrossSectionStyled>
 )
 
--- src/components/layout.js
+++ src/components/layout.js
@@ -13,38 +13,38 @@ import Header from "./header"
 import "./layout.css"
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
+    const data = useStaticQuery(graphql`
+        query SiteTitleQuery {
+            site {
+                siteMetadata {
+                    title
+                }
+            }
         }
-      }
-    }
-  `)
+    `)
 
-  return (
-    <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={
-          {
-            // margin: `0 auto`,
-            // maxWidth: 960,
-            // padding: `0px 1.0875rem 1.45rem`,
-            // paddingTop: 0,
-          }
-        }
-      >
-        <main>{children}</main>
-        <footer>© {new Date().getFullYear()}</footer>
-      </div>
-    </>
-  )
+    return (
+        <>
+            <Header siteTitle={data.site.siteMetadata.title} />
+            <div
+                style={
+                    {
+                        // margin: `0 auto`,
+                        // maxWidth: 960,
+                        // padding: `0px 1.0875rem 1.45rem`,
+                        // paddingTop: 0,
+                    }
+                }
+            >
+                <main>{children}</main>
+                {/* <footer>© {new Date().getFullYear()}</footer> */}
+            </div>
+        </>
+    )
 }
 
 Layout.propTypes = {
-  children: PropTypes.node.isRequired,
+    children: PropTypes.node.isRequired,
 }
 
 export default Layout
