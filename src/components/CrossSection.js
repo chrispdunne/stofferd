8c7ba7299a3f43675fe91b2a1feb59f4dd5ea893
Chris <chrispdunne@gmail.com>
2019-09-29 20:46:36 +0100

    style tweaks

--- src/components/CrossSection.js
+++ src/components/CrossSection.js
@@ -6,9 +6,12 @@ import Particles from "react-particles-js"
 
 import bgImg from "../images/isometic-underlay2.png"
 import fgImg from "../images/isometic-overlay2.png"
+import fg2Img from "../images/isometic-gardens2.png"
 
 const CrossSectionStyled = styled.div`
     overflow: hidden;
+    /* background-color: #5c5; */
+    background-color: #cd7;
     #pinContainer {
         height: 100vh;
         width: 100vw;
@@ -18,10 +21,10 @@ const CrossSectionStyled = styled.div`
             top: 50%;
             transform: translateY(-50%);
             right: 0rem;
-            text-align: right;
+            /* text-align: right; */
             background-color: rgba(0, 0, 0, 0.7);
-            padding: 1rem 1rem 1rem;
-            margin-right: 1rem;
+            padding: 1.5rem 1.2rem 1.5rem;
+            margin-right: 2rem;
             span {
                 color: #fff;
                 display: block;
@@ -29,23 +32,30 @@ const CrossSectionStyled = styled.div`
             }
         }
     }
-    .bg {
-        background-image: url(${bgImg});
+    .bg,
+    .fg,
+    .fg2 {
+        position: absolute;
+        top: 0;
         background-size: contain;
         background-repeat: no-repeat;
         /* background-position: center; */
         width: 100%;
         height: 100%;
     }
-    .fg {
-        position: absolute;
-        top: 0;
-        background-image: url(${fgImg});
+    .bg {
+        background-image: url(${bgImg});
         background-size: contain;
         background-repeat: no-repeat;
-        /* background-position: center; */
         width: 100%;
         height: 100%;
+    }
+    .fg {
+        background-image: url(${fgImg});
+    }
+    .fg2 {
+        background-image: url(${fg2Img});
+        z-index: 4;
     }
     .vignette {
         background: radial-gradient(rgba(255, 255, 255, 0), rgba(0, 0, 0, 0.6));
@@ -69,15 +79,20 @@ const CrossSection = () => (
         <Controller>
             <Scene triggerHook="onLeave" duration="200%" pin>
                 <Timeline wrapper={<div id="pinContainer" />}>
-                    <div className=" bg" />
+                    <div className="bg" />
 
-                    <Tween from={{ y: "0%" }} to={{ y: "-50%" }}>
+                    <div className="fg2" />
+                    <Tween from={{ y: "0%" }} to={{ y: "-30%" }}>
                         <div className=" fg" />
                     </Tween>
 
-                    <Tween from={{ opacity: "0" }} to={{ opacity: "1" }}>
+                    <Tween
+                        from={{ x: "50%", y: "-50%", opacity: "0" }}
+                        to={{ x: "0%", y: "-50%", opacity: "1" }}
+                    >
                         <section className="panel intro">
                             <span>
+                                <h3>Heading here</h3>
                                 This cut away look at a building rocks your
                                 socks so it does hear ye hear ye.
                             </span>
--- src/components/header.js
+++ src/components/header.js
@@ -1,36 +1,37 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import styled from "styled-components"
+
+const StyledHeader = styled.header`
+    position: fixed;
+    z-index: 3;
+    right: 2rem;
+    top: 2rem;
+`
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      // background: `red`,
-      marginBottom: `1.45rem`,
-      position: "absolute",
-      zIndex: "3",
-    }}
-  >
-    <h1 style={{}}>
-      <Link
-        to="/"
-        style={{
-          color: `#fff`,
-          textDecoration: `none`,
-        }}
-      >
-        {siteTitle}
-      </Link>
-    </h1>
-  </header>
+    <StyledHeader>
+        <h1 style={{}}>
+            <Link
+                to="/"
+                style={{
+                    color: `#fff`,
+                    textDecoration: `none`,
+                }}
+            >
+                {siteTitle}
+            </Link>
+        </h1>
+    </StyledHeader>
 )
 
 Header.propTypes = {
-  siteTitle: PropTypes.string,
+    siteTitle: PropTypes.string,
 }
 
 Header.defaultProps = {
-  siteTitle: ``,
+    siteTitle: ``,
 }
 
 export default Header
