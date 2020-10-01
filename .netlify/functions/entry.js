31f6e9ce25ed80d1fc6a31dc78912bf71ae4c50e
Chris <chrispdunne@gmail.com>
2020-10-01 21:11:24 +0100

    this is the one

--- .netlify/functions/entry.js
+++ .netlify/functions/entry.js
@@ -1,14 +1,10 @@
 exports.handler = async (event, context, callback) => {
-    // console.log(${process.env.GATSBY_PASSWORDS});
     const pwords = process.env.GATSBY_PASSWORDS
-    // const body = Buffer.from(event.body, "base64").toString()
-    const typed = JSON.parse(event.body.typed)
-    const match = pwords && pwords.split(" ").includes(typed)
+    const body = JSON.parse(event.body)
+    const match = pwords && pwords.split(" ").includes(body.typed)
 
     callback(null, {
         statusCode: 200,
         body: match,
-        // body: "match: " + pwords + " " + body,
-        // body: event.body,
     })
 }
