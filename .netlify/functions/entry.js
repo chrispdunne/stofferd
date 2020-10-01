9a3f1644283d5756b4ce80350916e2c457342bd6
Chris <chrispdunne@gmail.com>
2020-10-01 17:30:51 +0100

    var format

--- .netlify/functions/entry.js
+++ .netlify/functions/entry.js
@@ -1,6 +1,6 @@
 exports.handler = async (event, context, callback) => {
     // console.log(${process.env.GATSBY_PASSWORDS});
-    const pwords = ${process.env.GATSBY_PASSWORDS};
+    const pwords = process.env.GATSBY_PASSWORDS
     const match = pwords && pwords.split(" ").includes(event.body)
 
     callback(null, {
