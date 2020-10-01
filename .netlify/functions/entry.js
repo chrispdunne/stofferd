4808817f5ac2c4217d006d3d2dfcff64060ebb78
Chris <chrispdunne@gmail.com>
2020-10-01 17:50:22 +0100

    try formData

--- .netlify/functions/entry.js
+++ .netlify/functions/entry.js
@@ -5,6 +5,6 @@ exports.handler = async (event, context, callback) => {
 
     callback(null, {
         statusCode: 200,
-        body: "match: " + pwords + " " + event.body,
+        body: "match: " + pwords + " " + event.formData,
     })
 }
