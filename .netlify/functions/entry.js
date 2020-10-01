99d13f3e12cae5935ca44d6b3d2be8647f51277f
Chris <chrispdunne@gmail.com>
2020-10-01 21:08:13 +0100

    nearly there!?

--- .netlify/functions/entry.js
+++ .netlify/functions/entry.js
@@ -1,12 +1,14 @@
 exports.handler = async (event, context, callback) => {
     // console.log(${process.env.GATSBY_PASSWORDS});
     const pwords = process.env.GATSBY_PASSWORDS
-    const body = Buffer.from(event.body, "base64").toString()
-    const match = pwords && pwords.split(" ").includes(body)
+    // const body = Buffer.from(event.body, "base64").toString()
+    const typed = JSON.parse(event.body.typed)
+    const match = pwords && pwords.split(" ").includes(typed)
 
     callback(null, {
         statusCode: 200,
+        body: match,
         // body: "match: " + pwords + " " + body,
-        body: event.body,
+        // body: event.body,
     })
 }
