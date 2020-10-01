d69bebd5f11a17c432551d1429bdf661e20f6498
Chris <chrispdunne@gmail.com>
2020-10-01 20:36:23 +0100

    decode that sweeet base64

--- .netlify/functions/entry.js
+++ .netlify/functions/entry.js
@@ -1,7 +1,7 @@
 exports.handler = async (event, context, callback) => {
     // console.log(${process.env.GATSBY_PASSWORDS});
     const pwords = process.env.GATSBY_PASSWORDS
-    const body = Buffer.from(event.body, "base64").toString()
+    const body = Buffer.from(event.body, "base64").toString("ascii")
     const match = pwords && pwords.split(" ").includes(body)
 
     callback(null, {
