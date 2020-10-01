8a279568867c828bcf3ce5430644b93c2c1da616
Chris <chrispdunne@gmail.com>
2020-10-01 20:46:49 +0100

    decode!

--- .netlify/functions/entry.js
+++ .netlify/functions/entry.js
@@ -1,7 +1,7 @@
 exports.handler = async (event, context, callback) => {
     // console.log(${process.env.GATSBY_PASSWORDS});
     const pwords = process.env.GATSBY_PASSWORDS
-    const body = Buffer.from(event.body, "base64").toString("ascii")
+    const body = Buffer.from(event.body, "base64").toString("utf-8")
     const match = pwords && pwords.split(" ").includes(body)
 
     callback(null, {
